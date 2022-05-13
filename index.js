765e97158aa86ab749fa588b82ace5fdda893461
andrewrlee <1517745+andrewrlee@users.noreply.github.com>
2022-05-13 16:40:23 +0100

    Restructure packages

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import fs from "fs";
 
-import listSheduledJobs from "./github/index.js";
-import listPrs from "./circle/index.js";
-import listTicketsToTest from "./jira/index.js";
+import listPrs from "./outstandingPrs/index.js";
+import listSheduledJobs from "./failingScheduledJobs/index.js";
+import listTicketsToTest from "./ticketsToTest/index.js";
 
 const config = JSON.parse(fs.readFileSync("./config.json"));
 
 const projects = config.projects;
 
-await listSheduledJobs(config, projects);
-await listPrs(config, projects);
-await listTicketsToTest(config, projects);
+// await listSheduledJobs(config, projects);
+// await listPrs(config, projects);
+// await listTicketsToTest(config, projects);
